63b451b392d7c05a1a204f58ad5153b7bfb39105
jzheng-ria <jzheng@riainabox.com>
2023-01-24 23:41:46 -0500

    Feat: finished burnout blog post

--- src/components/Blogs/Burnout/Burnout.js
+++ src/components/Blogs/Burnout/Burnout.js
@@ -30,28 +30,34 @@ const Burnout = () => {
             <p>In <span className="emphasis">March of 2020</span>, I was told by my company that we would be issued laptops and begin working from home until further notice. It felt surreal. I didn&apos;t expect to be working from home so suddenly.</p>
             <p>Perhaps the biggest effect this pandemic has had on me was the work from home <span className="emphasis">(WFH)</span> situation.</p>
             <p>It was amazing. My commute to the office ranged anywhere from 2-3 hours roundtrip. It wasn&apos;t pleasant to say the least. The amount of time saved from commuting has paid for itself 10x over. I got more sleep and I was able to get more out of the day.</p>
-            <p>Of course, there are cons as well.</p>
-            <p>The one problem? I just didn&apos;t have much real-world experience. Who would want to hire someone with no actual coding experience in the web development field? I had tot think of a way to gain that experience.</p>
-            <p>Brainstorming ideas, I happened to stumble on a tutoring site called <a href="https://www.wyzant.com" target="_blank" rel="noopener noreferrer">Wyzant</a>. It was a site that allowed anyone to tutor students in a wide array of subjects such as programming as long as you passed a series of exams.</p>
-            <p>Why did this site catch my eye? Well I believe it&apos;s because being a mentor has always been a core value in my life. Throughout my entire life, I have been in numerous mentorship positions including volunteering at local elementary schools, providing tutoring to fellow classmates during my lunchtimes in high school, and being a TA in college.</p>
-            <p>So I figured why not? I&apos;ll sign-up and see where it takes me.</p>
+            <p>Of course, there were cons as well.</p>
+            <p>With all the time I saved not having to commute, I found it difficult allocating that time to the various parts of my life.</p>
+            <p>I didn&apos;t want to go out in fear of catching covid, so the majority of my time spent in 2021 was indoors. Besides work, the only other activity I dedicated a significant chunk of my time to was working out.</p>
+            <p>Equipped with only a set of adjustable dumbbells and a bench, I adjusted the routine I normally did at the gym and continued making progress.</p>
+            <p>It felt great knowing that covid didn&apos;t entirely prevent me from working out because I attribute a significant portion of phyiscal health and quite frankly my sanity to working out.</p>
+            <p>But besides working out, I didn&apos;t do much else in my spare time. Instead, I made the mistake (in hindsight) of opening up my work laptop and continuing on whatever task I had left off at. Slowly, this became my routine and before I knew it, the line between my personal time and work time was starting to blur.
+            </p>
 
-            <h2>The Start</h2>
-            <p>It was definitely a slow beginning. I don&apos;t think I had a single request from anyone for about a week. And then it started.</p>
-            <p>My first ever student on that site was a teacher who was teaching themselves web development and happened to stumble upon a fullstack curriculum that closely reflected what I had done back in my bootcamp.</p>
-            <p>As soon as I started tutoring again, it felt natural. It took me back to my past experiences and I instantly remembered why I had taken on all those past mentorship roles. I enjoy all of it, especially the cathartic feeling of being able to teach someone something and having all that finally click in their heads. Above all, I gain a sense of purpose when I tutor.</p>
-            <blockquote className="one-line-quote">
-              <p><span>Only a life lived for others is a life worthwhile.</span><span className="inline-author">-- Albert Einstein</span></p>
+            <h2>Breaking the Routine</h2>
+            <p>When I was working at the office, the line between my personal time and work time was always pretty clear. My work time starts as soon as I get into the office and ends once I leave. But with the pandemic and WFH, it had become a habit of mine to open up my work laptop whenever I couldn&apos;t find something else to do.</p>
+            <p>I worked constantly. After dinner? Work. Nights when I couldn&apos;t sleep? Work. My daily routine was almost all work without me even realizing it. It wasn&apos;t clear to me at the time that I was slowly <span className="emphasis">burning out.</span></p>
+            <blockquote>
+              <p>In order to break the cycle of burnout, it is important to observe and identify your burn events, prepare for those you can identify, and recover from each.</p>
+              <p className="author">-- Dr. Kate Steiner</p>
             </blockquote>
-            <p>This began my tutoring career. After my first student, more and more students came to me looking for help on their projects or assignments. By 3 months in, it had become a full time job where I was working more than 40-50 hours per week.</p>
+            <p>When 2022 rolled around and I had my annual review, the lead for my team had told me that I had contributed the most out of everyone. I was ectastic to say the least and my salary and bonus definitely reflected my efforts. But that was also when it truly hit me that I had spent way too much time focused on work. It was clear that I had to reestablish that boundary.</p>
+            <p>I made sure that once the work day ends, I would shut down my work laptop and force myself not to turn it on again until the next day. Luckily, things were slowly returning back to normal in 2022. Restaurants were starting to open up again, friends were more willing to reconnect in-person and slowly I felt my personal time coming back to me.</p>
+            {/* <blockquote className="one-line-quote">
+              <p><span>Only a life lived for others is a life worthwhile.</span><span className="inline-author">-- Albert Einstein</span></p>
+            </blockquote> */}
+            {/* <p>This began my tutoring career. After my first student, more and more students came to me looking for help on their projects or assignments. By 3 months in, it had become a full time job where I was working more than 40-50 hours per week.</p> */}
 
             <h2>Conclusion</h2>
-            <p>A key takeaway from all this is that life can really take you in any direction, and even the smallest action can have a big impact on where your life takes you.</p>
-            <p>To this day, I still tutor anywhere from 5-10 hours a week because I genuinely enjoy it.</p>
-            <blockquote>
-              <p>The very first thing Joey does is to discover the problem. Once that is discovered, he then explains possible solutions and then explains the process. This is great because I learned how to think when programming in either Java or React. I come out of each session with the confidence and encouragement to do more. At the end of the session, Joey takes the time to make sure all my questions are answered. I would highly recommend him if you have any web application questions.</p>
-              <p className="author">-- Mike</p>
-            </blockquote>
+            <p>Looking back, it wasn&apos;t the worst thing in the world having my work time slowly take over my personal time. I had gotten a lot of work done and as a result, also picked up a lot of new tools and concepts that will hopefully make me a better software engineer.</p>
+            <p>But like all things, it&apos;s important to balance your time and make sure that your physical and mental health are at the top of your priorities. </p>
+
+            <p>Burnout is all too common in my field of work and I hope that anyone who does experience it can find their own way of dealing with it.</p>
+
           </section>
         </section>
       </main>
